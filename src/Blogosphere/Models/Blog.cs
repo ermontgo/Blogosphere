0de7819f680911fadc3998e82daf88ae24feec2c
Eric Montgomery <ericm@remphoto.net>
2012-05-28 14:47:46 -0600

    Created initial models
    Added Author, CreatedDate, Post, Title, and ShortName to the post

---
+++ src/Blogosphere/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blogosphere.Models
+{
+    public class Blog
+    {
+        public string Author { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Post { get; set; }
+        public string Title { get; set; }
+        [Key]
+        public string ShortName { get; set; }
+    }
+}
