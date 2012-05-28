782b3133ecda26e24e26be97c55036f17ef4b0ca
Eric Montgomery <ericm@remphoto.net>
2012-05-28 16:11:38 -0600

    Added list view for blog posts

---
+++ src/Blogosphere/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blogosphere.Models;
+
+namespace Blogosphere.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext ctx = new BlogContext();
+
+        public ActionResult Index()
+        {
+            return View(ctx.Posts.ToList());
+        }
+    }
+}
