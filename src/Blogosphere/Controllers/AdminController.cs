6c8080d8d9af8d629cde939b79ca431583d7ed42
Eric Montgomery <ericm@remphoto.net>
2012-05-28 20:55:07 -0600

    Added auto-generated admin controller

---
+++ src/Blogosphere/Controllers/AdminController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blogosphere.Models;
+
+namespace Blogosphere.Controllers
+{
+    public class AdminController : Controller
+    {
+        private BlogContext db = new BlogContext();
+
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        //
+        // GET: /Admin/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            Blog blog = db.Posts.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // GET: /Admin/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Create
+
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+        //
+        // GET: /Admin/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            Blog blog = db.Posts.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // POST: /Admin/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+        //
+        // GET: /Admin/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            Blog blog = db.Posts.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // POST: /Admin/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Blog blog = db.Posts.Find(id);
+            db.Posts.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
